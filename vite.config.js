44451d06f41a54cae928c2cc5350f513f371d9be
Stephen882-pixel <stephenondeyo0@gmail.com>
2026-01-05 17:49:50 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/Tesla-Clone-with-ReactJS-Redux-Styled-Components-",
+  //base: process.env.VITE_BASE_PATH || "/Tesla-Clone-with-ReactJS-Redux-Styled-Components-",
   plugins: [
     react({
       babel: {
